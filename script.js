44328f08a624cb77c556dbe75952e473444fccfa
Sumit Rajpurohit <118529312+SumitRajpurohit@users.noreply.github.com>
2023-06-27 14:09:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+var element = document.getElementById("level");
+var level = 0;
+while (element.parentNode) {
+    element = element.parentNode;
+    level++;
+}
+
+alert("The level of the element is: " + level);
